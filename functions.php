b76744c54b89f0be1991a74c6cf544b8bfcf23fc
sebastiandanac <sebastiandanac@gmail.com>
2021-07-24 13:51:50 +0200

    anothe rtry

--- functions.php
+++ functions.php
@@ -49,20 +49,12 @@ function load_css()
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/styles/colors.css', array(), );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/styles/colors.css', array('parent-style') );
 
-	$parent_style = 'parent-style'; 
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    ); 
-
-
 }
 add_action('wp_enqueue_scripts', 'load_css');
  
-
+add_action( 'wp_enqueue_scripts', function() { 
+	wp_enqueue_style( 'colors', get_template_directory_uri() . '/styles/colors.css' );
+});
 
 //lightbox
 
