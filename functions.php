464956350e55416dfd670543238c05b2cd57ea4b
sebastiandanac <sebastiandanac@gmail.com>
2021-07-24 13:40:22 +0200

    child style priority

--- functions.php
+++ functions.php
@@ -56,6 +56,11 @@ function load_css()
 }
 add_action('wp_enqueue_scripts', 'load_css');
 
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles', 11 );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'child-style', get_stylesheet_uri() );
+}  
+
 
 //lightbox
 
