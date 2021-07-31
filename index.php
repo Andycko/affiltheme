905edfb3754c071e6f36403c797a53a4bf9aa3ac
sebastiandanac <sebastiandanac@gmail.com>
2021-07-31 16:24:38 +0200

    new index

--- index.php
+++ index.php
@@ -1,24 +1,7 @@
-<?php get_header(); ?>
+<?php 
 
-    <div class="main-page-blog-top">
-
-        <div class="main-page-blog-top-inner">
-            <img class="main-page-blog-top-img" src="" alt="">
-            <h1 class="main-page-blog-top-head"><br class="index-res-break-h1"></h1>
-        </div>
-
-    </div>
-
-    <div class="main-page-blog-inner">
-
-        <?php include("template-parts/blog-posts-lists.php") ?>
-
-        <aside class="main-page-blog-inner-block main-page-blog-inner-right">
-
-            <?php get_sidebar(); ?>
-
-        </aside>
-
-    </div>
-
-<?php get_footer(); ?>
+$context          = Timber::context();
+$context['posts'] = new Timber\PostQuery();
+$context['foo']   = 'bar';
+$templates        = array( 'index.twig' );
+Timber::render( $templates, $context );     
